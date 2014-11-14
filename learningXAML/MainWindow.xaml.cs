a8d4de6c625ee1a00f088fcd14edb558c1685156
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-11-14 17:53:25 -0600

    Built list of people as an exercise
    Included a SQLite DB and allowed for more details to be seen when items
    are selected.

---
+++ learningXAML/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace learningXAML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            learningXAML.peopleDataSet peopleDataSet = ((learningXAML.peopleDataSet)(this.FindResource("peopleDataSet")));
+
+            // Load data into the table sample_data. You can modify this code as needed.
+            learningXAML.peopleDataSetTableAdapters.sample_dataTableAdapter peopleDataSetsample_dataTableAdapter = new learningXAML.peopleDataSetTableAdapters.sample_dataTableAdapter();
+            peopleDataSetsample_dataTableAdapter.Fill(peopleDataSet.sample_data);
+            System.Windows.Data.CollectionViewSource sample_dataViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("sample_dataViewSource")));
+            sample_dataViewSource.View.MoveCurrentToFirst();
+        }
+
+    }
+}
